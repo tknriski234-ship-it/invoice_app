1a151f268a6287b8031458399e7e9ef418f375e0
tknriski234-ship-it <rizky@DESKTOP-EK1RB5Q.localdomain>
2026-03-27 03:11:23 +0700

    fixxed bug

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -9,4 +9,4 @@ def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    # return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
